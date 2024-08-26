5f2ae05cdeb3a08a944bd95976b15cfe2682ad05
Karson Tang (TECHVIFY.D2) <karson.tang@techvify.com.vn>
2024-08-26 17:18:35 +0700

    Initial project

---
+++ api/src/main/java/vn/com/lol/blitzcrack/BlitzcrackApplication.java
@@ -0,0 +1,13 @@
+package vn.com.lol.blitzcrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlitzcrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlitzcrackApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/vn/com/lol/blitzcrack/BlitzcrackApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.com.lol.blitzcrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlitzcrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
